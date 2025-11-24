53df6a48e12f2375dde925481105dfce5c20bef9
Rahul-Muthuswamy <rahul.m.muthuswamy@gmail.com>
2025-11-24 11:43:37 +0530

    ✨ feat[backend]:Implement backend routes for additional functions in MORE

--- backend/threaddit/__init__.py
+++ backend/threaddit/__init__.py
@@ -26,8 +26,6 @@ CORS(
     origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
         "http://localhost:5000",
         "http://127.0.0.1:5000",
         "http://localhost:3000",
